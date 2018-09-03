21beb59275988bd572efb1cd8205505f7a2f4f4c
it-school <it@osvita.od.ua>
2018-09-03 12:11:22 +0300

    Initial commit

---
+++ src/com/itschool/Main.java
@@ -0,0 +1,14 @@
+package com.itschool;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a);
+        int b = 6;
+        System.out.println(b);
+        int c = a + b;
+        System.out.println(c);
+
+    }
+}
